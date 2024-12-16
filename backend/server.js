b6f6b986d74932700e343323d8849271cde6dcef
bethuel vba <bethuel2122@gmail.com>
2024-12-16 10:22:30 +0100

    Mise en production

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
 import cors from "cors"
+import path from "path"
 
 import authRouter from "./routers/auth.route.js";
 import messageRouter from "./routers/message.route.js";
@@ -10,6 +11,7 @@ import userRouter from "./routers/user.route.js";
 import connectDB from "./db/connect.db.js";
 import { app, server } from "./socket/socket.js";
 
+const __dirname = path.resolve()
 
 
 dotenv.config()
@@ -18,9 +20,14 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
 app.use(cors({
     origin: "*"
 }))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 app.use("/api/auth",authRouter);
 app.use("/api/messages",messageRouter);
